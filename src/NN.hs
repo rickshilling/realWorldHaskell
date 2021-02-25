529709424d5ae13f2b8010bf6f1f12923c76a6e9
rickshilling <rick.shilling@gmail.com>
2021-02-24 19:59:40 -0500

    Add files via upload

---
+++ src/NN.hs
@@ -0,0 +1,15 @@
+module NN
+    ( 
+    ) where
+import Control.Monad
+import Data.Functor
+import Data.List
+--import System.Random
+
+{-
+gauss :: Float -> IO Float
+gauss stdev = do
+  x<-randomIO
+  y<-randomIO
+  return $stdev*sqrt(-2*log x)*cos(2*pi*y)
+-}
